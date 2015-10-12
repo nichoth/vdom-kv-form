f061fe7e15ceeada5f21a9bd84ae5a82b23e96e7
nichoth <nichoth@gmail.com>
2015-10-12 09:29:48 -0700

    don't add rows if last is empty

--- KVForm.js
+++ KVForm.js
@@ -2,6 +2,7 @@ var h = require('virtual-dom/h');
 var state = require('@nichoth/state');
 var map = require('lodash.map');
 var oArray = require('observ-array');
+var value = require('observ');
 var KVInput = require('vdom-kv-input');
 var noop = function(){};
 
@@ -9,37 +10,63 @@ module.exports = KVForm;
 
 function KVForm(opts) {
 
+  var rows = oArray( (opts.rows || []).map(function(r, i) {
+    return KVInput({
+      field: r.field,
+      value: r.value,
+      onDelete: onDelete(i),
+      onComplete: onComplete()
+    });
+  }));
+
   var s = state({
-    rows: oArray( (opts.rows || []).map(function(r, i) {
-      return KVInput({
-        field: r.field,
-        value: r.value,
-        onDelete: onDelete(i),
-        onComplete: onComplete()
-      });
-    }))
+    rows: rows,
+    lastRowIsEmpty: value( KVForm.lastRowIsEmpty({rows: rows}) )
   });
 
 
   function onDelete(index) {
     return function() {
       s.rows.splice(index, 1);
+      s.lastRowIsEmpty.set(KVForm.lastRowIsEmpty(s));
     };
   }
 
   function onComplete() {
     return function() {
-      s.rows.push( KVInput({
-        onComplete: onComplete(),
-        onDelete: onDelete(s.rows().length)
-      }));
+
+      if (!s.lastRowIsEmpty()) {
+        addRow(s, {
+          onComplete: onComplete(),
+          onDelete: onDelete(s.rows().length)
+        });
+
+        s.lastRowIsEmpty.set(KVForm.lastRowIsEmpty(s));
+      }
+
     };
   }
 
   return s;
 }
 
+
+function addRow(state, data) {
+  state.rows.push( KVInput({
+    onComplete: data.onComplete,
+    onDelete: data.onDelete
+  }));
+}
+
+
+KVForm.lastRowIsEmpty = function(state) {
+  var rs = state.rows();
+  return rs.length && !( KVInput.isComplete(state.rows.get(rs.length-1)) );
+};
+
+
 KVForm.render = function render(state) {
+  console.log(state);
   return h('form.vdom-kv-form', {
     onsubmit: function(ev) {
       ev.preventDefault();
